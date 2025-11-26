8a1b70624abc1b3f5bc5f5424eb3faa70b17b50e
Rashmit Pankhania <rashmitpankhania@gmail.com>
2025-11-27 01:15:07 +0530

    Remove main method from TcpEchoClient to focus on IClient interface implementation

--- src/main/java/com/rashmit/johncrickett/Tcp/TcpEchoClient.java
+++ src/main/java/com/rashmit/johncrickett/Tcp/TcpEchoClient.java
@@ -8,26 +8,6 @@ import java.net.Socket;
 import com.rashmit.johncrickett.Interfaces.IClient;
 
 public class TcpEchoClient implements IClient {
-    public static void main(String[] args) {
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-
-        try (
-                Socket socket = new Socket(hostName, portNumber);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
-
-            System.out.println("connected at:" + socket.getLocalPort());
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo: " + in.readLine());
-            }
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
-        }
-    }
 
     @Override
     public void start(String hostName, int portNumber) {
